8f51e90d56199b5b1e53c6c6761066a8d4b0c8cc
Gonzalo Petraglia <gonzalo.petraglia@gmail.com>
2021-02-03 22:00:58 -0300

    checkout mp
    checkout mp

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser')
 
-var app = express();
+const port = process.env.PORT || 3000
+
+const app = express();
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
  
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
-});
+app.use(require('./routes'));
 
 app.listen(port);
---
+++ routes.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const mercadopago = require ('mercadopago');
+require('./config/config');
+
+mercadopago.configure({
+    access_token: ACCESS_TOKEN,
+    integrator_id: INTEGRATOR_ID,
+});
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.render('home', {view: 'home'});
+});
+
+app.get('/detail', function (req, res) {
+    res.render('detail', Object.assign({view: 'item'}, req.query));
+});
+
+app.post('/checkout', function (req, res) {
+    let preference = {
+        items: [{
+            title: req.body.title,
+            unit_price: Number(req.body.price),
+            quantity: 1, //Number(req.body.unit),
+            picture_url: path.join(BASE_URL + req.body.img),
+            id: '1234',
+            description: 'Dispositivo móvil de Tienda e-commerce'
+        }],
+        external_reference: 'gpetraglia@quaresitsolutions.com',
+        payer:{
+            name: "Lalo",
+            surname: "Landa",
+            email: "test_user_63274575@testuser.com",
+            phone: {
+                area_code: '11',
+                number: 22223333
+            },
+            address: {
+                zip_code: '1111',
+                street_name: 'False',
+                street_number: 123
+            }
+        },
+        back_urls: {
+            "success": BASE_URL + '/success',
+            "failure": BASE_URL + '/failure',
+            "pending": BASE_URL + '/pending'
+        },
+        auto_return: 'approved',
+        payment_methods: {
+            excluded_payment_methods: [
+                {
+                    id: "amex"
+                }
+            ],
+            excluded_payment_types: [
+                {
+                    id: "atm"
+                }
+            ],
+            installments: 6
+        },
+        notification_url: BASE_URL + '/notification'
+    };
+
+
+    mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.redirect(response.body.init_point);
+		}).catch(function (error) {
+			console.log(error);
+        });
+
+});
+
+
+app.get('/success', function (req, res) {
+    if(req.query.payment_id){
+        let data = {
+            view: '',
+            message: 'Gracias por su compra, el pago se efectuo con exito.',
+            payment: {
+                id: req.query.payment_id,
+                external_reference: req.query.external_reference,
+                merchant_order_id: req.query.merchant_order_id,
+            }
+        }
+        res.render('message', data);
+    }
+    else {
+        res.redirect('/');
+    }
+});
+
+app.get('/failure', function (req, res) {
+    if(req.query.payment_id){
+        let data = {
+            view: '',
+            message: 'Ocurrio un error al efectuar el pago, por favor reintentar.',
+            payment: {
+                id: req.query.payment_id,
+                external_reference: req.query.external_reference,
+                merchant_order_id: req.query.merchant_order_id,
+            }
+        }
+        res.render('message', data);
+    }
+    else {
+        res.redirect('/');
+    }
+});
+
+app.get('/pending', function (req, res) {
+    if(req.query.payment_id){
+        let data = {
+            view: '',
+            message: 'Gracias por su compra, el pago se encuentra pendiente.',
+            payment: {
+                id: req.query.payment_id,
+                external_reference: req.query.external_reference,
+                merchant_order_id: req.query.merchant_order_id,
+            }
+        }
+        res.render('message', data);
+    }
+    else {
+        res.redirect('/');
+    }
+});
+
+app.post('/notification', function (req, res) {
+    fs.writeFileSync('./log.json', JSON.stringify(req.body));
+    res.status(200).json({});
+});
+
+app.get('/log', function (req, res) {
+    let log = require('./log.json');
+    res.json(log);
+});
+
+
+module.exports = app
